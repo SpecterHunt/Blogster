ff1b7bde40d672228dc88229f13eabc52fcf6623
SpecterHunt <ayusht420@gmail.com>
2023-03-16 17:39:41 +0530

    Travis config

---
+++ config/ci.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+module.exports = {
+  googleClientID:
+    "149299586610-1q3cnr1nrggqcan42kvdclpa3g380os2.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
+  cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379",
+};
